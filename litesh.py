c05cf48dfe8eef7679ee591d3bd1c8e7b69ddb84
beastbybirth <113232311+beastbybirth@users.noreply.github.com>
2022-12-20 15:58:37 +0530

    Update litesh.py

--- litesh.py
+++ litesh.py
@@ -1,21 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)    #setting BOARD as pin numbering system
 
+#declaring all the pins
 led = 12
 trigpin = 11
 echopin = 7
 
-GPIO.setup(trigpin, GPIO.OUT)
-GPIO.setup(echopin, GPIO.IN)
-GPIO.setup(led, GPIO.OUT)
+GPIO.setup(trigpin, GPIO.OUT)   #setting trig pins to output
+GPIO.setup(echopin, GPIO.IN)    #setting echo pin to input
+GPIO.setup(led, GPIO.OUT)       #initialising the led
 
-pwm = GPIO.PWM(led, 100)
+pwm = GPIO.PWM(led, 100)    #setting up the instance for pwm
 pwm.start(0)
 
-def dist():
-    
+def dist():     #function to calculate the distance
     GPIO.output(trigpin, GPIO.HIGH) 
     time.sleep(0.0001) 
     GPIO.output(trigpin, GPIO.LOW)
@@ -27,9 +27,9 @@ def dist():
     while GPIO.input(echopin) == 1:
         stop = time.time()
 
-    totalTime = stop - start
+    totalTime = stop - start        #calculating total time
     
-    dist = (totalTime * 34300) / 2
+    dist = (totalTime * 34300) / 2  #calculating distance
     
     return dist
 
@@ -37,10 +37,10 @@ def dist():
 while True:
     distance = dist()
     if (distance <= 50):
-        pwm.ChangeDutyCycle(50 - distance)
+        pwm.ChangeDutyCycle(50 - distance)  #changing the brightness of led according to distance
         time.sleep(0.5)
         
     else :
-        pwm.ChangeDutyCycle(0)
+        pwm.ChangeDutyCycle(0)      #else turn off the led
     time.sleep(0.001)
         
